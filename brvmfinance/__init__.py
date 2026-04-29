f4235b4bb176e77e824404cd847a968bb101b02a
sora <moussa.sissoko6@etu.univ-lorraine.fr>
2026-04-29 15:28:56 +0000

    config: Add setup

--- brvmfinance/__init__.py
+++ brvmfinance/__init__.py
@@ -1,2 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# brvmfinance - market data downloader for BRVM
+#
+
 from .ticker import Ticker
-from .scrapers.scrapers import fetch_history
+from .tickers import Tickers
+
+__version__ = "0.1.0"
+__author__ = "soradata"
+
+# from brvmfinance import *
+__all__ = ['Ticker', 'Tickers']
--- setup.py
+++ setup.py
@@ -1,11 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import io
+from os import path
 from setuptools import setup, find_packages
 
+# --- Résolution du chemin pour le README ---
+here = path.abspath(path.dirname(__file__))
+
+
+try:
+    with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = "Un outil Python pour télécharger les données boursières de la BRVM."
+
 setup(
-    name="brvmfinance",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        "pandas",
-        "requests",
+    name='brvmfinance',
+    version='0.1.0',
+    description='Download market data from BRVM (Bourse Régionale des Valeurs Mobilières)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/SORADATA/brvmfinance',
+    author='Ton Nom',
+    author_email='ton.email@example.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
+    platforms=['any'],
+    keywords='pandas, brvm, finance, afrique, bourse, sika finance',
+    packages=find_packages(exclude=['tests', 'docs', 'examples']),
+    install_requires=[
+        'pandas>=1.0.0',
+        'requests>=2.20.0'
+    ],
+    include_package_data=True,
 )
+
+# --- Le Disclaimer (Très important quand on fait du Web Scraping) ---
+print("""
+====================================================================
+NOTE: `brvmfinance` n'est pas affilié, soutenu ou validé par la
+BRVM (Bourse Régionale des Valeurs Mobilières) ni par SikaFinance.
+
+Cet outil est fourni à des fins éducatives et de recherche.
+Vous devez vous référer aux conditions d'utilisation des sources
+d'origine concernant vos droits d'utilisation des données téléchargées.
+====================================================================
+""")
